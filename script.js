3e1d37215202a58f41e269b177f4cda1daf0ff32
maruchanmusic <189743388+maruchanmusic@users.noreply.github.com>
2025-06-22 21:13:51 +0900

    Add playGame function
    playGame function plays 5 rounds and shows final results

--- script.js
+++ script.js
@@ -66,3 +66,24 @@ const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+
+function playGame() {   
+   for(let i = 1; i <= 5; i++) {
+     const humanSelection = getHumanChoice();
+     const computerSelection = getComputerChoice();
+     playRound(humanSelection, computerSelection); 
+   }
+}
+playGame();
+
+
+console.log(`Good game! You won ${humanScore} of 5 matches`);
+if (humanScore > computerScore) {
+    console.log("Congratulations. You are the winner!!!")
+}
+else if (humanScore < computerScore) {
+    console.log("Sorry...you lost the game :(")
+}
+else {
+    console.log("It seems like it is a draw! Let's play again!")
+}
